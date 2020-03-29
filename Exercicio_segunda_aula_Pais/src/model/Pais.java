f6b32cfd70872ac2ffbd35cd1a1b12ddeb7fead7
leopdani <51030652+leopdani@users.noreply.github.com>
2020-03-29 11:23:16 -0300

    Add files via upload

---
+++ Exercicio_segunda_aula_Pais/src/model/Pais.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.io.Serializable;
+
+public class Pais implements Serializable {
+    private static final long serialVersionUID = 1L;
+	private int id;
+	private String nome;
+	private long populacao;
+	private double area;
+
+	public Pais() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getPopulacao() {
+		return populacao;
+	}
+
+	public void setPopulacao(long populacao) {
+		this.populacao = populacao;
+	}
+
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+	@Override
+	public String toString() {
+		return "Pais [id=" + id + ", nome=" + nome + ", populaca=" + populacao + ", area=" + area + "]";
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pais other = (Pais) obj;
+		if (area == -1) {
+			if (other.area != -1)
+				return false;
+		} else if (area ==(other.area))
+			return false;
+		if (populacao == -1) {
+			if (other.populacao != -1)
+				return false;
+		} else if (populacao==(other.populacao))
+			return false;
+		if (id != other.id)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+}
